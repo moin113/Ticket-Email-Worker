3e1ea713af3171f138ae4c3e3480056ffa5d46a3
moin113 <80636315+moin113@users.noreply.github.com>
2026-02-16 00:20:45 +0530

    Email log Succesfully fixed

---
+++ TicketEmailWorker/Model/EmailLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketEmailWorker.Model
+{
+    public class EmailLog
+    {
+        public string CCEmail { get; set; } = string.Empty;
+        public string BccEmail { get; set; } = string.Empty;
+        public string ToEmail { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string FromEmail { get; set; } = string.Empty;
+
+        public DateTime SentAt { get; set; } 
+
+    }
+}
